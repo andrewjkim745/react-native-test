50d9489807c2709b1ef9300cfc61babff3b7bf59
Andrew J Kim <andrewk745@gmail.com>
2020-05-14 20:15:18 -0400

    did more testing with react native

--- App.js
+++ App.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
+import FlexContainer from './components/flexContainer'
 
 
 const RaisedButton = props => <Button raised {...props} />;
 
-export default function App() {
+const renderFlexContainer = e => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <RaisedButton 
-      title="Press Me" 
-      buttonStyle={styles.button}
-      type="outline"
-      />
-    </View>
-  );
+    <FlexContainer />
+  )
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+
+    this.state = {
+      display: false,
+    }
+  }
+
+
+  handleDisplay = e => {
+    this.setState({
+      display: true
+    })
+  }
+
+
+  render() {
+    return (
+      <>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <RaisedButton
+            title="Press Me"
+            buttonStyle={styles.button}
+            type="outline"
+            onPress={this.handleDisplay}
+          />
+        </View>
+        {display ? <FlexContainer/> : <View/>}
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
