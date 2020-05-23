df798ea864ecbb63a06bc091b5b08722786e7c17
Andrew J Kim <andrewk745@gmail.com>
2020-05-23 19:57:29 -0400

    fixed some issues

--- components/DailyWeather.js
+++ components/DailyWeather.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 
 
 
 export const DailyWeather = ({ minTemp, maxTemp, dayTemp, feelsLike, weather }) => {
     return (
-        <View>
-            <Text>Weather</Text>
+        <View style={styles.dailyWeatherContainer}>       
+            <Text>{dayTemp}</Text>
+            <Text>{minTemp}</Text>
+            <Text>{maxTemp}</Text>
+            <Text>{feelsLike}</Text>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    dailyWeatherContainer : {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
